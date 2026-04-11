23b4c645b987110a32026e0cc22b54585c5e542a
Mohamed Talaat <mohamedtalattt5@gmail.com>
2026-04-11 20:32:35 +0200

    ...

--- ECommerce.Infrastructure/Implementation/PaymentService.cs
+++ ECommerce.Infrastructure/Implementation/PaymentService.cs
@@ -1,60 +1,60 @@
-﻿using ECommerce.Application.Repositories;
-using ECommerce.Application.Repositories.Contract.Common;
-using ECommerce.Application.Services;
-using ECommerce.Domain.Entities;
-using Microsoft.Extensions.Configuration;
-using Stripe;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Product = ECommerce.Domain.Entities.Product;
+﻿//using ECommerce.Application.Repositories;
+//using ECommerce.Application.Repositories.Contract.Common;
+//using ECommerce.Application.Services;
+//using ECommerce.Domain.Entities;
+//using Microsoft.Extensions.Configuration;
+//using Stripe;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Product = ECommerce.Domain.Entities.Product;
 
-namespace ECommerce.Infrastructure.Implementation
-{
-    public class PaymentService : IPaymentService
-    {
-        private readonly IBasketRepository _basketRepository;
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly IConfiguration _config;
+//namespace ECommerce.Infrastructure.Implementation
+//{
+//    public class PaymentService : IPaymentService
+//    {
+//        private readonly IBasketRepository _basketRepository;
+//        private readonly IUnitOfWork _unitOfWork;
+//        private readonly IConfiguration _config;
 
-        public PaymentService(IConfiguration config,IUnitOfWork unitOfWork , IBasketRepository basketRepository)
-        {
-            _basketRepository = basketRepository;
-            _unitOfWork = unitOfWork;
-            _config = config;
-        }
+//        public PaymentService(IConfiguration config,IUnitOfWork unitOfWork , IBasketRepository basketRepository)
+//        {
+//            _basketRepository = basketRepository;
+//            _unitOfWork = unitOfWork;
+//            _config = config;
+//        }
 
-        public async Task<CustomerBasket> CreateOrUpdatePaymentIntent(string basketId)
-        {
-            StripeConfiguration.ApiKey = _config["SripeSettings:SecretKey"];
-            var basket = await _basketRepository.GetBasketAsync(basketId);
-            if (basket == null) return null;
+//        public async Task<CustomerBasket> CreateOrUpdatePaymentIntent(string basketId)
+//        {
+//            StripeConfiguration.ApiKey = _config["SripeSettings:SecretKey"];
+//            var basket = await _basketRepository.GetBasketAsync(basketId);
+//            if (basket == null) return null;
 
-            decimal shippingPrice = 0m;
+//            decimal shippingPrice = 0m;
 
-            if(basket.DeliveryMethodId.HasValue)
-            {
-                var deliveryMethod = await _unitOfWork.Repository<DeliveryMethod>().GetByIdAsync(basket.DeliveryMethodId.Value);
-                shippingPrice = deliveryMethod.Price;
-            }
+//            if(basket.DeliveryMethodId.HasValue)
+//            {
+//                var deliveryMethod = await _unitOfWork.Repository<DeliveryMethod>().GetByIdAsync(basket.DeliveryMethodId.Value);
+//                shippingPrice = deliveryMethod.Price;
+//            }
 
-            foreach(var item in basket.Items)
-            {
-                var productItem = await _unitOfWork.Repository<Product>().GetByIdAsync(item.Id);
-                if(item.Price != productItem.Price)
-                {
-                    item.Price = productItem.Price; // لو حصل تغير في السعرفي الفرونت بالغلط مثلا لا حدث دائما باللي موجود في الداتا بيز
-                }
+//            foreach(var item in basket.Items)
+//            {
+//                var productItem = await _unitOfWork.Repository<Product>().GetByIdAsync(item.Id);
+//                if(item.Price != productItem.Price)
+//                {
+//                    item.Price = productItem.Price; // لو حصل تغير في السعرفي الفرونت بالغلط مثلا لا حدث دائما باللي موجود في الداتا بيز
+//                }
 
-                var service = new PaymentIntentCreateOptions
-                {
-                    Amount = (long)basket.Items.Sum(i=>i.Quantity *(i.Price*100))+(long)shippingPrice*100,
-                    Currency = "usd",
-                    Payment
-                }
-            }
-        }
-    }
-}
+//                var service = new PaymentIntentCreateOptions
+//                {
+//                    Amount = (long)basket.Items.Sum(i=>i.Quantity *(i.Price*100))+(long)shippingPrice*100,
+//                    Currency = "usd",
+//                    Payment
+//                }
+//            }
+//        }
+//    }
+//}
